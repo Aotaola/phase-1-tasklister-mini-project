e0905eaa228229b14ecfddf96563443e25d28edc
Alejandro <alejelias@gmail.com>
2022-11-26 15:33:45 -0500

    final js. with duedate stretch goal

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-
-
   const createtaskform = document.getElementById("create-task-form")
   const description = document.getElementById("new-task-description")
-  const createnewtaskform = document.querySelector("submit")
+  // const createnewtaskform = document.querySelector("submit")
+  const dueDate = document.getElementById("due-date");
   const ulGoal = document.getElementById("tasks")
+
+
 
   createtaskform.addEventListener("submit", (e) => {
     e.preventDefault();
     var list = document.createElement("li")
-    ulGoal.appendChild(list);
-    list.innerText = description.value;
-    // description.appendChild(list);this one line messed with my head for hours 
+    let deleteButton = document.createElement("button")
+    //var x = document.getElementById("due-date").value;
+    deleteButton.innerHTML = "Delete"
+    ulGoal.append(list, dueDate.value, deleteButton);
+    list.innerText = description.value; 
+    //dueDate.value; this can be deleted later
+    // description.appendChild(list); this one line messed with my head for hours
     description.value = "";
-    createnewtaskform.submit();
-    list.addEventListener("remove", (e) => {
-      ulGoal.removeChild(list);
-})});
+    dueDate.value = "";
+    // createtaskform.submit();
+    deleteButton.addEventListener("click", (e) => {
+      ulGoal.remove()
+      //ulGoal.removeChild(list,dueDate);
+      //ulGoal.removeChild(dueDate.value);
+      // dueDate.value.remove();
+      //deleteButton.remove()
+  })
+ });
 })
-
-
-  
