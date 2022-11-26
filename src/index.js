862fa2cc3ebe318390c527fcb91573d3c5c7ab32
Alejandro <alejelias@gmail.com>
2022-11-26 12:01:21 -0500

    updating tasklistener project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+  const createtaskform = document.getElementById("create-task-form")
+  const description = document.getElementById("new-task-description")
+  const createnewtaskform = document.querySelector("submit")
+  const ulGoal = document.getElementById("tasks")
+
+  createtaskform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var list = document.createElement("li")
+    ulGoal.appendChild(list);
+    list.innerText = description.value;
+    // description.appendChild(list);this one line messed with my head for hours 
+    description.value = "";
+    createnewtaskform.submit();
+    list.addEventListener("remove", (e) => {
+      ulGoal.removeChild(list);
+})});
+})
+
+
+  
